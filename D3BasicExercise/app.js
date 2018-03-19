528cd4f911ce1e60cd3e38b38801f1ad2f2dd91e
Samuel Meyer <info@samueldmeyer.com>
2018-03-18 21:06:20 -0700

    Updated to D3 v4, made exercise use scales

---
+++ D3BasicExercise/app.js
@@ -0,0 +1,66 @@
+'use strict';
+$(function() {
+  // Declare your chart variable and connect it to the #barchart in your HTML
+  // Give it a width of 200 and a height of 240
+    // Why is the height 240? 
+      // In our sample CSV, we have 11 values.
+      // Later on, we'll set each bar to have a height of 20 with a space of 2.
+      // The last bar will not have the space of 2.
+      // 10*22 + 20 = 240
+    // It is translated 40 pixels down to leave room for axis labels
+  var chart = d3.select('#barchart').attr('transform', 'translate(40,0)')
+  // CODE HERE
+  // CODE HERE
+  // CODE HERE
+
+  // Set up an xScale (scaleLinear) with a domain from 0 to 100 and a range from 0 to 200
+    // CODE HERE
+  var xScale
+  // HINT:
+  // console.log('xScale(10):' xScale(10));
+  // should print 'xScale(10): 20'
+
+  // Uncomment this line to add an axis label:
+  // chart.append('g').attr('class', 'x axis').call(d3.axisTop(xScale));
+
+
+  // Instead of loading in data from within the doc, such as...
+    // var data = [ 1, 2, 3, 4, 5, 4, 3, 4, 3, 2, 1 ];
+  // ...let's try loading in the data from the included CSV using d3.csv
+  d3.csv("D3Sample.csv", function(error, data) {
+    if(error) {
+      console.log('error: ', error);
+      return;
+    }
+    // Now let's bind that data to our SVG.
+    chart.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      // Let's add in the following attributes:
+        // The y position of each rectangle will be based on the value's index position,
+        // with a space of 2 between each rectangle (for a total difference of 22)
+        // Each rectangle will have a height of 20.
+        // The rectangles will have an x position of 0.
+        // At the beginning, the rectangles will have a width of 0 so that they will "grow in" through animation.
+        // but for now, set their width to xScale(parseFloat(d.population)) so that you can see the chart.
+      // CODE HERE
+      // CODE HERE
+      // CODE HERE
+      // CODE HERE
+
+      // Once a static chart works for you, uncomment the code block below, and set width to 0 above:
+      // .transition()
+      // //   Here we will configure the attributes for our transition animation:
+      // //     The delay will be based on each value's index position, staggered by a multiple of 100 of the index to create a "one-by-one" effect.
+      // //       https://github.com/d3/d3-transition/blob/master/README.md#timing
+      // //     The duration will be constant and set at 200ms.
+      // //     The width for each rectangle will be set to the final value: xScale(parseFloat(d.population))
+      // //     The value is loaded as a String from the csv, so we convert it with parseFloat()
+      // // CODE HERE
+      // // CODE HERE
+      // // CODE HERE
+      // // CODE HERE
+
+  });
+});
--- README.md
+++ README.md
@@ -17,20 +17,20 @@ For both of these exercises you will need to run a python local server. You will
 5. Once you have the server up and running, in your browser, go to the link `0.0.0.0:8000/` to view your code in action.
 6. By default the server serves the `index.html` in your folder to the browser. You can open other files from the same folder in the browser by appending the file name to the default address. For e.g. `0.0.0.0:8000/solutions.html` to open the `solutions.html` file from the same folder.
 
-Once your server is up, start with the exercises. Don't forget to use your browser developer tools for debugging! 
+Once your server is up, start with the exercises in `app.js`. Don't forget to use your browser developer tools for debugging! 
 
-**Note : After making changes to your file, if they do not reflect on your browser : restart the server and clear your browser cache.**
+**Note : After making changes to your file, if they do not reflect on your browser: do a hard refresh by Shift+click on the refresh button.**
 
 ## D3 Basic Exercise (start with this)
 
-1. Open the `index.html` file and follow along with the comments to add the missing code and get the visualization working.
+1. Open the `index.html` file and follow along with the comments in `app.js` to add the missing code and get the visualization working.
 2. As you go through, try to figure out what is going on with the code. Ask the TAs if you need help. 
-3. When you are done (or if you are really stuck!) look at the `solution.html` file.
+3. When you are done (or if you are really stuck!) look at the `app_solution.js` file.
 4. If you get the basic visualization working, you can make your own modifications (add animations and interactivity, change colors, etc.) or move on to the advanced exercise below.
 
 ## D3 Advanced Exercise 
 
-If you already have some experience in D3 or have completed the beginner exercise. Try your hand at this more advanced exercise. The original idea is from a Spring 2015 assignment by David Eicke.
+If you already have some experience in D3 or have completed the beginner exercise. Try your hand at this more advanced exercise. The original idea is from a Spring 2015 assignment by David Eicke. However, it has not been updated to use D3 v4, so... anyone who contributes that update will be appreciated.
 
 1. Read through David's code (original.html) and look at the visualization in your browser to get a sense of what is going on in the code.
 2. Implement this code using the billboardtop5.csv data.
